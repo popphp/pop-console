5b763561e421e4035db107f8181df4d9046a84f8
Nick Sagona <nick@nolainteractive.com>
2016-12-02 15:10:34 -0600

    Update docblocks

--- src/Exception.php
+++ src/Exception.php
@@ -17,10 +17,10 @@ namespace Pop\Console;
  * Pop console exception class
  *
  * @category   Pop
- * @package    Pop_Console
+ * @package    Pop\Console
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
- * @version    2.1.0
+ * @version    3.0.0
  */
 class Exception extends \Exception {}
