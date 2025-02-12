b20bfdb752d238c9da7957b685ff6832382a28e1
Nick Sagona <nick@noladev.com>
2025-02-12 16:20:08 -0600

    Update version

--- src/Color.php
+++ src/Color.php
@@ -21,7 +21,7 @@ namespace Pop\Console;
  * @author     Nick Sagona, III <dev@noladev.com>
  * @copyright  Copyright (c) 2009-2025 NOLA Interactive, LLC.
  * @license    https://www.popphp.org/license     New BSD License
- * @version    4.2.3
+ * @version    4.2.4
  */
 class Color
 {
